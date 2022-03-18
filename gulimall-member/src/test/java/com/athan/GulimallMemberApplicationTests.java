cba1453eb2eaf16b5f37e292f5922826443d0159
athan <dongdong980327@163.com>
2022-03-18 14:31:28 +0800

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/athan/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.athan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
